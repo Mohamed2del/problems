6a2be1ac9b77383f8a26908bbc5bf9d1469e1289
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2021-07-18 12:16:44 +0200

    Problem7 without freq solved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,17 +2,21 @@ package com.company;
 
 
 import com.company.solved.Problem11;
+import com.company.solved.Problem7;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
 
-        Problem11 p = new Problem11(10);
-        p.run();
+        Problem7 p = new Problem7();
 
-        }
+        p.newsolution("sessxx".replaceAll(" ",""));
+
+    }
 
 
 
--- src/com/company/solved/Problem7.java
+++ src/com/company/solved/Problem7.java
@@ -48,6 +48,12 @@ public class Problem7 {
     }
 
 
+    public void newsolution(String s){
+            Map<Character, Integer> map = new HashMap<>();
+            s.chars().forEach(e->map.put((char)e, map.getOrDefault((char)e, 0) + 1));
+            System.out.println(map);
+    }
+
     // freq
     public  void countFrequencies(ArrayList<String> list)
     {
