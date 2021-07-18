9f8a8cfae2d72243a48ecf931ccde1abf06ec050
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2021-07-18 11:42:58 +0200

    Problem11 solved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,9 @@
 package com.company;
 
 
-import com.company.problem10.Machine;
-import com.company.problem10.Problem10;
-import com.company.solved.Problem4;
+import com.company.solved.Problem11;
 
-import java.lang.constant.Constable;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.text.NumberFormat;
-import java.util.Currency;
-import java.util.Locale;
 
 public class Main {
 
