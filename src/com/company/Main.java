119169bd91c184b5905a47a7a213517714cedfa1
Mohamed Adel <Mohamedadelmohamed997@gmail.com>
2021-07-18 11:42:33 +0200

    Problem11 solved

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,28 +8,16 @@ import com.company.solved.Problem4;
 import java.lang.constant.Constable;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-//
-//
-//        Problem10 p = new Problem10();
-//        Constructor objectConstuctor = p.f("Computer",new Class[]{String.class});
-//
-//
-//        Object instance = objectConstuctor.newInstance("Computer");
-//
-//        System.out.println(instance.getClass().getName());
-        String s ;
-        Problem10 p = new Problem10();
-        Object object = p.f("VideoGame");
 
-        Machine machine =(Machine) object;
-        System.out.println(machine.getName());
-//        Machine machine = (Machine)object;
-//        machine.toString();
-//        System.out.println(object.getClass().getName());
+        Problem11 p = new Problem11(10);
+        p.run();
 
         }
 
--- src/com/company/Problem11.java
+++ src/com/company/Problem11.java
@@ -1,28 +1,45 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /*
 Write a program to execute in parallel N calls to a method which sleep 1 second, for
 example if N is 10 so method will print any string 10 times in one second
  */
-public class Problem11  extends Thread {
+public class Problem11 implements Runnable {
+    int n ;
+    ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-
-    public void printAnyString() throws InterruptedException  {
-        System.out.println("Any String");
-        Thread.sleep(1000);
+    public Problem11(int n){
+        this.n = n;
     }
-
-    public void run(){
+    Runnable task = () -> {
+        System.out.println("Executing Task1 inside : " + Thread.currentThread().getName());
         try {
-            task(10);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException ex) {
+            throw new IllegalStateException(ex);
         }
+    };
+
+    public List<Runnable> listTasks(int n){
+        List tasks = new ArrayList<Runnable>();
+        for (int i = 0; i < n ; i++) {
+            tasks.add(executorService.submit(task));
+        }
+        return tasks;
     }
 
-    public void task(int n) throws InterruptedException {
-        for (int i = 0; i < n; i++) {
-            printAnyString();
-        }
+
+    @Override
+    public void run() {
+        listTasks(n);
+        executorService.shutdown();
+
     }
 }
+
